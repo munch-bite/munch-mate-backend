0fe0677cb96e209d52ab8546031755e5f468b3a0
menniia <mennia17@gmail.com>
2024-08-17 15:00:10 +0000

    updated files

---
+++ controllers/cartController.js
@@ -0,0 +1,20 @@
+
+
+
+// add to cart
+const addToCart = async (req, res, next) => {
+
+}
+
+
+// remove items from cart
+const removeFromCart = async (req, res, next) => {
+
+}
+
+// get cart data
+const getCartData = async (req, res, next) => {
+
+}
+
+export { addToCart, removeFromCart, getCartData }
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import usersRoute from "./routes/usersRoute.js";
 import foodRoute from "./routes/foodRoute.js";
 import vendorsRoute from "./routes/vendorsRoute.js";
 import ordersRoute from "./routes/ordersRoute.js";
+import cartRoute from "./routes/cartRoute.js";
 
 await mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Mongo DB connected"))
@@ -42,6 +43,7 @@ app.use("/api/v1", usersRoute);
 app.use("/api/v1", vendorsRoute);
 app.use("/api/v1", foodRoute);
 app.use("/api/v1", ordersRoute);
+app.use("/api/v1", cartRoute);
 
 expressOasGenerator.handleRequests();
 app.use((req, res) => res.redirect("/api-docs/"));
