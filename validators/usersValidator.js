5315f05ecb93524466b7e1602939b6e5eeadd6a7
menniia <mennia17@gmail.com>
2024-08-19 19:15:14 +0000

    code fixes

--- validators/usersValidator.js
+++ validators/usersValidator.js
@@ -4,13 +4,12 @@ export const usersValidator = joi.object({
     firstName: joi.string().required(),
     middleName: joi.string(),
     lastName: joi.string().required(),
-    userName: joi.string(),
     businessName: joi.string().min(3).max(50),
     address: joi.string(),
     location: joi.string(),
     email: joi.string().email().required(),
     phoneNumber: joi.string(),
     role: joi.string().valid("customer", "vendor").required(),
-    password: joi.string().min(4),
+    password: joi.string().min(4).required(),
     confirmPassword: joi.ref("password")
 }).with("password", "confirmPassword");
